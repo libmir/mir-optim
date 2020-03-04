2874eefd58bc5be50bc0b6ed9769feee92873cb6
9il <ilyayaroshenko@gmail.com>
2020-03-04 14:27:26 +0700

    wip

--- source/mir/optim/internal.d
+++ source/mir/optim/internal.d
@@ -2,8 +2,10 @@ module mir.optim.internal;
 
 import mir.ndslice;
 
+static import lapack;
+public import lapack: lapackint;
 import mir.blas;
-import mir.utility: min;
+import mir.utility: min, max;
 
 alias T = double;
 
@@ -85,7 +87,8 @@ do {
     y.mulBy(eigenValuesRoots);
 }
 
-@safe pure nothrow @nogc
+import std.stdio;
+// @safe pure nothrow @nogc
 void eigenSqrtSolve(
     Slice!(const(T)*, 2, Canonical) eigenVectors,
     Slice!(const(T)*) eigenValuesRoots,
@@ -102,6 +105,7 @@ in {
 do {
     auto r = eigenValuesRoots.length;
     y.divBy(eigenValuesRoots);
+    debug writeln("invroots = ", 1 / eigenValuesRoots);
     gemv!T(1, eigenVectors[0 .. r].transposed, y, 0, x);
 }
 
@@ -195,6 +199,70 @@ do {
     gemv!T(1, leftSingularVectors[0 .. $, 0 .. r].transposed, y, 0, temp);
     temp.divBy(singularValues);
     gemv!T(alpha, rightSingularVectors[0 .. $, 0 .. r], temp, 0, x);
+}
+
+version(LAPACK_STD_COMPLEX)
+{
+    import std.complex: Complex;
+    alias _cfloat = Complex!float;
+    alias _cdouble = Complex!double;
+}
+else
+{
+    alias _cfloat = cfloat;
+    alias _cdouble = cdouble;
+}
+
+
+/// Solves a real symmetric indefinite system of linear equations AX=B,
+/// using the factorization computed by SSPTRF.
+extern(C) @system @nogc nothrow
+{
+void ssytrs_3_(ref const char uplo, ref const lapackint n, ref const lapackint nrhs, const(float) *a, ref const lapackint lda, const(float)* e, const(lapackint)* ipiv, float* b, ref const lapackint ldb, ref lapackint info);
+void dsytrs_3_(ref const char uplo, ref const lapackint n, ref const lapackint nrhs, const(double) *a, ref const lapackint lda, const(double)* e, const(lapackint)* ipiv, double* b, ref const lapackint ldb, ref lapackint info);
+void csytrs_3_(ref const char uplo, ref const lapackint n, ref const lapackint nrhs, const(_cfloat) *a, ref const lapackint lda, const(_cfloat)* e, const(lapackint)* ipiv, _cfloat* b, ref const lapackint ldb, ref lapackint info);
+void zsytrs_3_(ref const char uplo, ref const lapackint n, ref const lapackint nrhs, const(_cdouble) *a, ref const lapackint lda, const(_cdouble)* e, const(lapackint)* ipiv, _cdouble* b, ref const lapackint ldb, ref lapackint info);
+
+}
+
+alias sytrs_3_ = ssytrs_3_;
+alias sytrs_3_ = dsytrs_3_;
+alias sytrs_3_ = csytrs_3_;
+alias sytrs_3_ = zsytrs_3_;
+
+///
+size_t sytrs_3(T)(
+    char uplo,
+    Slice!(const(T)*, 2, Canonical) a,
+    Slice!(const(T)*) e,
+    Slice!(const(lapackint)*) ipiv,
+    Slice!(T*, 2, Canonical) b,
+    )
+in
+{
+    assert(a.length!0 == a.length!1, "sytrs_3: 'a' must be a square matrix.");
+    assert(e.length == a.length, "sytrs_3: 'e' must have the same length as 'a'.");
+    assert(b.length!1 == a.length, "sytrs_3: 'b.length!1' must must be equal to 'a.length'.");
+}
+do
+{
+    lapackint n = cast(lapackint) a.length;
+    lapackint nrhs = cast(lapackint) b.length;
+    lapackint lda = cast(lapackint) a._stride.max(1);
+    lapackint ldb = cast(lapackint) b._stride.max(1);
+    lapackint info = void;
+// ref char uplo, ref lapackint n, ref lapackint nrhs, float *a, ref lapackint lda, float* e, lapackint *ipiv, float *b, ref lapackint ldb, ref lapackint info
+    sytrs_3_(uplo, n, nrhs, a.iterator, lda, e.iterator, ipiv.iterator, b.iterator, ldb, info);
+    assert(info >= 0);
+    return info;
+}
+
+unittest
+{
+    alias s = sytrs_3!float;
+    alias d = sytrs_3!double;
+    alias c = sytrs_3!cfloat;
+    alias z = sytrs_3!cdouble;
 }
 
 version(none):
@@ -296,41 +364,6 @@ unittest
 }
 
 ///
-size_t sytrs_3(T)(
-    char uplo,
-    Slice!(T*, 2, Canonical) a,
-    Slice!(T*) e,
-    Slice!(lapackint*) ipiv,
-    Slice!(T*, 2, Canonical) b,
-    Slice!(T*) work,
-    )
-in
-{
-    assert(a.length!0 == a.length!1, "sytrs_3: 'a' must be a square matrix.");
-    assert(e.length == a.length, "sytrs_3: 'e' must have the same length as 'a'.");
-    assert(b.length!1 == a.length, "sytrs_3: 'b.length!1' must must be equal to 'a.length'.");
-}
-do
-{
-    lapackint n = cast(lapackint) a.length;
-    lapackint nrhs = cast(lapackint) b.length;
-    lapackint lda = cast(lapackint) a._stride.max(1);
-    lapackint ldb = cast(lapackint) b._stride.max(1);
-    lapackint info = void;
-    lapack.sytrs_3_(uplo, n, nrhs, a.iterator, lda, e.iterator, ipiv.iterator, b.iterator, ldb, work.iterator, info);
-    assert(info >= 0);
-    return info;
-}
-
-unittest
-{
-    alias s = sytrs_3!float;
-    alias d = sytrs_3!double;
-    alias c = sytrs_3!cfloat;
-    alias z = sytrs_3!cdouble;
-}
-
-///
 auto iamax(T,
     SliceKind kindX,
     )(
@@ -357,144 +390,4 @@ auto infNormDistance(T)(Slice!(const(T)*) x, Slice!(const(T)*) y)
 auto faminElem(T, SliceKind kind)(Slice!(const(T)*, 1, kind) x)
 {
     return reduce!fmin(+T.infinity, x.map!fabs);
-}
-
-struct QPSolver(T)
-    if (is(T == float) || is(T == double))
-{
-    import mir.math.common: sqrt, fmin, fmax;
-    import mir.math.constant: GoldenRatio;
-
-    T sigma = T.epsilon.sqrt;
-    T rho = 1;
-    T alpha = GoldenRatio;
-    T epsRel = T.epsilon.sqrt.sqrt * 3;
-    T epsAbs = T.epsilon.sqrt.sqrt * 3;
-    T epsPrimalInfeasibility = T.epsilon.sqrt.sqrt;
-    T epsDualInfeasibility = T.epsilon.sqrt.sqrt;
-
-    size_t n;
-    size_t m;
-
-    Slice!(T, 1, Universal) sigmaI() @trusted @property
-    {
-        return typeof(return)(&sigma, [n], [sizediff_t(1)]);
-    }
-
-    Slice!(T*, 2) A;  // n x m
-    Slice!(T*, 2) P; // P n
-    Slice!(T*, 2) F; // LDL^t factorization of (P + σI + ρA^tA)
-
-    Slice!(T*) q; // n
-    Slice!(T*) l; // n
-    Slice!(T*) u; // n
-
-    Slice!(lapackint*) F_ipiv;
-    Slice!(T*) F_work;
-    Slice!(T*) F_e; // n
-
-    Slice!(T*) x; // n
-    Slice!(T*) x_prev; // n
-    Slice!(T*, 4) buff_4n; // 2 * n
-
-    Slice!(T*) y; // m
-    Slice!(T*) y_prev; // m
-    Slice!(T*) z; // m
-    Slice!(T*) Ax; // m
-    Slice!(T*) Ax_prev; // m
-    Slice!(T*) buff_m; // m
-
-    void updateF()
-    {
-        mir.blas.copy!T(P.flattened, F.flattened);
-        mir.blas.axpy!T(1, sigmaI, F.diagonal);
-        mir.blas.syrk!T(Uplo.lower, rho, A.transposed, 1, F);
-        sytrf_rk!T('U', F, F_e, F_ipiv, F_work);
-    }
-
-    QPTerminationCriteria iterate()
-    {
-        mir.blas.copy!T(y, y_prev);
-        mir.blas.scale!T(-1, y);
-        mir.blas.axpy!T(rho, z, y);
-        mir.blas.copy!T(x, x_prev);
-        if (useA)
-            mir.blas.gemv!T(1, A.transposed, y, 0, x);
-        else
-            mir.blas.copy!T(y, x);
-        mir.blas.axpy!T(-1, q, x);
-        mir.blas.axpy!T(sigma, x_prev, x);
-        if (useA || useP)
-            sytrs_3!T('U', F, F_e, F_ipiv, x(1, n));
-        else
-            mir.blas.scale(1 / (1 + sigma + rho), x);
-        mir.blas.scale!T(alpha, x);
-        mir.blas.scale!T(1 - alpha, z);
-        mir.blas.copy!T(Ax, Ax_prev);
-        mir.blas.scale!T(1 - alpha, Ax);
-        if (useA)
-            mir.blas.gemv!T(1, A, x, 0, y);
-        else
-            mir.blas.copy!T(x, y);
-        mir.blas.axpy!T(1, y, z);
-        mir.blas.axpy!T(1, y, Ax);
-        mir.blas.axpy!T(1 - alpha, x_prev, x);
-        mir.blas.copy!T(z, y);
-        mir.blas.axpy!T(1 / rho, y_prev, z);
-        foreach (i, ref e; z.field)
-            e = e.fmax(l[i]).fmin(u[i]);
-        mir.blas.axpy!T(-1, z, y);
-        mir.blas.scale!T(rho, y);
-        mir.blas.axpy!T(1, y_prev, y);
-
-        mir.blas.copy!T(Ax, buff_4n[0]);
-        mir.blas.axpy!T(-1, z, buff_4n[0]);
-        if (famaxElem(buff_4n[0]) < epsAbs + epsRel * famaxElem(z).fmax(famaxElem(Ax)))
-            return QPTerminationCriteria.primal;
-        return QPTerminationCriteria.none;
-    }
-
-    QPTerminationCriteria criteria()
-    {
-        if (useA)
-        {
-            mir.blas.copy!T(x, buff_4n[1]);
-            mir.blas.axpy!T(-1, x_prev, buff_4n[1]);
-            if (useP)
-                mir.blas.symm!T(Side.right, Uplo.lower, 1, P, buff_4n[0 .. 2], 0, buff_4n[2 .. 4]);
-            else
-                mir.blas.copy(buff_4n[0 .. 2].flattened, 0, buff_4n[2 .. 4].flattened);
-            mir.blas.gemv!T(1, A.transposed, y, 0, buff_4n[0]);
-        }
-        else
-        {
-            if (useP)
-                mir.blas.symv!T(Uplo.lower, 1, P, x, 0, buff_4n[2]);
-            else
-                mir.blas.copy(x, buff_4n[2]);
-            mir.blas.copy!T(y, buff_4n[0]);
-        }
-
-        T prim = famaxElem(q)
-            .fmax(famaxElem(buff_4n[2]))
-            .fmax(famaxElem(buff_4n[0]));
-        mir.blas.axpy(1, q, buff_4n[0]);
-        mir.blas.axpy(1, buff_4n[2], buff_4n[0]);
-
-        if (famaxElem(buff_4n[0]) < epsAbs + epsRel * prim)
-            return QPTerminationCriteria.dual;
-
-        if (useA)
-        {
-            T normPdx = famaxElem(buff_4n[2] - buff_4n[3]);
-        }
-
-        return QPTerminationCriteria.none;
-    }
-
-
-    // uses buff_n as new #x
-    void update()
-    {
-    }
 }
--- source/mir/optim/qp.d
+++ source/mir/optim/qp.d
@@ -8,12 +8,15 @@ static import cblas;
 import lapack: lapackint;
 import mir.optim.internal;
 
+import mir.math.sum;
 import mir.utility: min, max;
 import mir.math.common: fmin, fmax, fabs, sqrt;
 import mir.math.constant: GoldenRatio;
 import mir.blas: gemv;
 import mir.internal.memory;
+import mir.blas;
 import mir.lapack;
+import cblas;
 
 /++
 +/
@@ -27,6 +30,9 @@ struct QPSolverSettings
     T epsRel = T.epsilon.sqrt.sqrt;
     /++
     +/
+    T epsInfeasibility = T.epsilon.sqrt.sqrt;
+    /++
+    +/
     T initialRho = T(1) / 8;
     /++
     +/
@@ -34,6 +40,12 @@ struct QPSolverSettings
     /++
     +/
     T maxRho = 1 / T.epsilon.sqrt; 
+    /++
+    +/
+    T sigma = T.epsilon.sqrt;
+    /++
+    +/
+    T alpha = GoldenRatio;
     /++
     +/
     bool adaptiveRho = true;
@@ -63,6 +75,151 @@ enum QPTerminationCriteria
     maxIterations,
 }
 
+///
+void symv(T,
+    SliceKind kindA,
+    SliceKind kindX,
+    SliceKind kindY,
+    )(
+    Uplo uplo,
+    T alpha,
+    Slice!(const(T)*, 2, kindA) a,
+    Slice!(const(T)*, 1, kindX) x,
+    T beta,
+    Slice!(T*, 1, kindY) y,
+    )
+{
+    assert(a.length!0 == a.length!1);
+    assert(a.length!1 == x.length);
+    assert(a.length!0 == y.length);
+    static if (kindA == Universal)
+    {
+        bool transA;
+        if (a._stride!1 != 1)
+        {
+            a = a.transposed;
+            transA = true;
+        }
+        assert(a._stride!1 == 1, "Matrix A must have a stride equal to 1.");
+    }
+    else
+        enum transA = false;
+    cblas.symv(
+        transA ? cblas.Order.ColMajor : cblas.Order.RowMajor,
+        uplo,
+        
+        cast(cblas.blasint) x.length,
+
+        alpha,
+
+        a.iterator,
+        cast(cblas.blasint) a.matrixStride,
+
+        x.iterator,
+        cast(cblas.blasint) x._stride,
+
+        beta,
+
+        y.iterator,
+        cast(cblas.blasint) y._stride,
+    );
+}
+
+///
+void symm(T,
+    SliceKind kindA,
+    SliceKind kindB,
+    SliceKind kindC,
+    )(
+    Side side,
+    Uplo uplo,
+    T alpha,
+    Slice!(const(T)*, 2, kindA) a,
+    Slice!(const(T)*, 2, kindB) b,
+    T beta,
+    Slice!(T*, 2, kindC) c,
+    )
+{
+    assert(a.length!1 == a.length!0);
+    assert(a.length!1 == b.length!0);
+    assert(a.length!0 == c.length!0);
+    assert(c.length!1 == b.length!1);
+    auto k = cast(cblas.blasint) a.length!1;
+
+    static if (kindC == Universal)
+    {
+        if (c._stride!1 != 1)
+        {
+            assert(c._stride!0 == 1, "Matrix C must have a stride equal to 1.");
+            .symm(
+                side == cblas.Side.Left ? cblas.Side.Right : cblas.Side.Left,
+                uplo == cblas.Uplo.Upper ? cblas.Uplo.Lower : cblas.Uplo.Upper,
+                alpha,
+                a.transposed,
+                b.transposed,
+                beta,
+                c.transposed.assumeCanonical);
+            return;
+        }
+        assert(c._stride!1 == 1, "Matrix C must have a stride equal to 1.");
+    }
+    static if (kindA == Universal)
+    {
+        bool transA;
+        if (a._stride!1 != 1)
+        {
+            a = a.transposed;
+            transA = true;
+        }
+        assert(a._stride!1 == 1, "Matrix A must have a stride equal to 1.");
+    }
+    else
+        enum transA = false;
+    static if (kindB == Universal)
+    {
+        bool transB;
+        if (b._stride!1 != 1)
+        {
+            b = b.transposed;
+            transB = true;
+        }
+        assert(b._stride!1 == 1, "Matrix B must have a stride equal to 1.");
+    }
+    else
+        enum transB = false;
+
+    cblas.symm(
+        cblas.Order.RowMajor,
+        transA ? cblas.Transpose.Trans : cblas.Transpose.NoTrans,
+        transB ? cblas.Transpose.Trans : cblas.Transpose.NoTrans,
+        
+        cast(cblas.blasint) c.length!0,
+        cast(cblas.blasint) c.length!1, 
+        cast(cblas.blasint) k,
+
+        alpha,
+
+        a.iterator,
+        cast(cblas.blasint) a.matrixStride,
+
+        b.iterator,
+        cast(cblas.blasint) b.matrixStride,
+
+        beta,
+
+        c.iterator,
+        cast(cblas.blasint) c.matrixStride,
+    );
+}
+
+
+private auto matrixStride(S)(S a)
+ if (S.N == 2)
+{
+    assert(a._stride!1 == 1);
+    return a._stride != 1 ? a._stride : a.length!1;
+}
+
 /++@safe pure nothrow @nogc+/
 // QPTerminationCriteria solveQuickQP(
 //     scope ref const QPSolverSettings settings,
@@ -81,6 +238,178 @@ enum QPTerminationCriteria
 //     ) /++@safe pure nothrow @nogc+/ infeasibilityTolerance = null
 // ) 
 // {
+
+struct QPSolver
+{
+    import mir.math.common: sqrt, fmin, fmax;
+    import mir.math.constant: GoldenRatio;
+
+    QPSolverSettings settings;
+
+    bool useA;
+    bool useP;
+
+    T qMax;
+
+    size_t n;
+    size_t m;
+
+    Slice!(T*, 2) A;  // n x m
+    Slice!(T*, 2) P; // P n
+    Slice!(T*, 2) F; // LDL^t factorization of (P + σI + ρA^tA)
+
+    Slice!(T*) q; // n
+    Slice!(T*) l; // n
+    Slice!(T*) u; // n
+
+    Slice!(lapackint*) F_ipiv;
+    Slice!(T*) F_work;
+    Slice!(T*) F_e; // n
+
+    Slice!(T*) x; // n
+    Slice!(T*) xPrev; // n
+    Slice!(T*) ATy; // n
+    Slice!(T*) ATyPrev; // n
+    Slice!(T*) Px; // n
+    Slice!(T*) PxPrev; // n
+    Slice!(T*, 4) buff_4n; // 2 * n
+
+    Slice!(T*) y; // m
+    Slice!(T*) yPrev; // m
+    Slice!(T*) z; // m
+    Slice!(T*) Ax; // m
+    Slice!(T*) AxPrev; // m
+    Slice!(T*) buff_m; // m
+
+    // void updateF()
+    // {
+    //     mir.blas.copy!T(P.flattened, F.flattened);
+    //     mir.blas.axpy!T(1, sigma, F.diagonal);
+    //     mir.blas.syrk!T(Uplo.lower, rho, A.transposed, 1, F);
+    //     sytrf_rk!T('U', F, F_e, F_ipiv, F_work);
+    // }
+
+    QPTerminationCriteria iterate()
+    {
+        auto rho = settings.initialRho;
+        with(settings) foreach (iters; 0 .. maxIterations)
+        {
+            xPrev[] = x;
+            yPrev[] = y;
+            y[] -= rho * z;
+            if (useA)
+                gemv!T(-1, A.transposed, y, sigma, x);
+            else
+                x[] = sigma * x - y;
+            x[] -= q;
+            sytrs_3!T('U', F.canonical, F_e, F_ipiv, x.sliced(x.length, 1).canonical);
+            x[] *= alpha;
+            AxPrev[] = Ax;
+            if (useA)
+                gemv!T(1, A, x, 0, Ax);
+            else
+                Ax[] = x;
+            y[] = Ax + (1 - alpha) * z + 1 / rho * y;
+            z[] = y.zip(l).map!fmax.zip(u).map!fmin;
+            y[] = rho * (y - z);
+            Ax[] += (1 - alpha) * AxPrev;
+
+            if ((iters + 1) % 6 && iters + 1 != maxIterations)
+                continue;
+
+            if (useA)
+            {
+                ATyPrev[] = ATy;
+                PxPrev[] = Px;
+                // TODO: use single gemm call
+                gemv!T(1, A.transposed, y, 0, ATy);
+                gemv!T(1, A.transposed, yPrev, 0, ATyPrev);
+            }
+            else
+            {
+                ATy[] = y;
+            }
+            if (useP)
+            {
+                // TODO: use single symm call
+                symv!T(Uplo.Lower, 1, P, x, 0, Px);
+                if (useA)
+                    symv!T(Uplo.Lower, 1, P, xPrev, 0, PxPrev);
+            }
+            else
+            {
+                Px[] = x;
+                if (useA)
+                    PxPrev[] = xPrev;
+            }
+            T AxMax = T(0).reduce!fmax(map!fabs(Ax));
+            T PxMax = T(0).reduce!fmax(map!fabs(Px));
+            T zMax = T(0).reduce!fmax(map!fabs(z));
+            T ATyMax = T(0).reduce!fmax(map!fabs(ATy));
+            T primResidual = T(0).reduce!fmax(map!fabs(Ax - z));
+            T primScale = fmax(AxMax, zMax);
+            T primResidualNormalized = primResidual ? primResidual / primScale : 0;
+            T dualResidual = T(0).reduce!fmax(map!fabs(Px + ATy + q));
+            T dualScale = fmax(qMax, fmax(PxMax, ATyMax));
+            T dualResidualNormalized = dualResidual ? dualResidual / dualScale : 0;
+
+            T epsPrim = epsAbs + epsRel * primScale;
+            T epsDual = epsAbs + epsRel * dualScale;
+            if (primResidual <= epsPrim && dualResidual <= epsDual)
+                return QPTerminationCriteria.solved;
+
+            if (useA)
+            {
+                // check primal infeasibility
+                yPrev[] -= y;
+                T dyMax = T(0).reduce!fmax(map!fabs(yPrev));
+                T dATyMax = T(0).reduce!fmax(map!fabs(ATy - ATyPrev));
+                T epsPrimInfeasibility = epsInfeasibility * dyMax;
+
+                if (dATyMax <= epsPrimInfeasibility)
+                {
+                    if (map!((d, ref u, ref l) => d * (d > 0 ? fmin(u, T.max.sqrt) : fmax(l, -T.max.sqrt)))(zip(yPrev, u, l)).sum!"fast" <= epsPrimInfeasibility)
+                        return QPTerminationCriteria.primalInfeasibility;
+                }
+
+                xPrev[] -= x;
+                T dxMax = T(0).reduce!fmax(map!fabs(xPrev));
+                T dPxMax = T(0).reduce!fmax(map!fabs(Px - PxPrev));
+                T epsDualInfeasibility = epsInfeasibility * dxMax;
+
+                if (dPxMax <= epsDualInfeasibility && dot!T(q, xPrev) <= epsDualInfeasibility)
+                {
+                    foreach(i; 0 .. m)
+                    {
+                        auto dAxi = Ax[i] - AxPrev[i];
+                        if (u[i] != +T.infinity)
+                            if ((dAxi <= +epsDualInfeasibility))
+                                goto F;
+                        if (l[i] != -T.infinity)
+                            if ((dAxi >= -epsDualInfeasibility))
+                                goto F;
+                    }
+                    return QPTerminationCriteria.dualInfeasibility;
+                }
+                F:
+            }
+
+            // if (adaptiveRho && ++rhoAge >= minAdaptiveRhoAge)
+            // {
+            //     T newRho = rho * sqrt(primResidualNormalized / dualResidualNormalized);
+            //     if (fmax(rho, newRho) > 5 * fmin(rho, newRho))
+            //     {
+            //         z[] = x;
+            //         rho = newRho;
+            //           rhoAge = 0;
+            //     }
+            // }
+        }
+        return QPTerminationCriteria.maxIterations;
+    }
+}
+
+version(none):
 
 QPTerminationCriteria solveQP(
     Slice!(const(T)*, 2, Canonical) P,
@@ -126,21 +455,21 @@ do {
 
 unittest
 {
-    auto P = [
-         2.0, -1, 0,
-        -1, 2, -1,
-         0, -1, 2,
-    ].sliced(3, 3);
-
     // auto P = [
-    //      1.0, -0, 0,
-    //     -0, 1, -0,
-    //      0, -0, 1,
+    //      2.0, -1, 0,
+    //     -1, 2, -1,
+    //      0, -1, 2,
     // ].sliced(3, 3);
+
+    auto P = [
+         1.0, -0, 0,
+        -0, 1, -0,
+         0, -0, 1,
+    ].sliced(3, 3);
 
     auto q = [3.0, -7, 5].sliced;
     auto l = [-100.0, -2, 1].sliced;
-    auto u = [100.0, 2, 1].sliced;
+    auto u = [100.0, 100, 1].sliced;
     auto x = slice!double(3);
     solveQuickQP(P.canonical, q, l, u, x);
     writeln(x);
@@ -177,8 +506,8 @@ do {
     auto z = work[0 .. n]; work = work[n .. $];
     eigenSolve(P_eigenVectors, P_eigenValues, q, work, z, -1);
     projection(l, u, z, x);
-    if (z == x)
-        return QPTerminationCriteria.solved;
+    // if (z == x)
+    //     return QPTerminationCriteria.solved;
     x[] = 0;
     y[] = 0;
     auto ret = approxSolveQuickQP(settings,
@@ -250,7 +579,7 @@ QPTerminationCriteria approxSolveQP(
         Slice!(const(T)*) xScaledPrev,
         Slice!(const(T)*) yScaled,
         Slice!(const(T)*) yScaledPrev,
-    ) /++@safe pure nothrow @nogc+/ infeasibilityTolerance = null
+    ) /++@safe pure \nothrow @nogc+/ infeasibilityTolerance = null
 ) 
 in {
     assert(projection);
@@ -370,10 +699,10 @@ do {
             // use innerZ as temporal storage
             innerZ[] = innerX;
     // debug writeln("approxSolveQuickQP#.innerX = ", innerX);
-    // debug writeln("approxSolveQuickQP#.innerZ = ", innerZ);
+    debug writeln("approxSolveQuickQP#.innerZ0 = ", innerZ);
             eigenSqrtSolve(P_eigenVectors, eroots, innerZ, x);
-    // debug writeln("approxSolveQuickQP#.innerZ = ", innerZ);
-    // debug writeln("approxSolveQuickQP#.x = ", x);
+    debug writeln("approxSolveQuickQP#.innerZ1 = ", innerZ);
+    debug writeln("approxSolveQuickQP#.x = ", x);
             projection(x, z); // set Z
             eigenSqrtTimes(P_eigenVectors, eroots, z, innerZ);
         },
@@ -405,6 +734,7 @@ QPTerminationCriteria approxSolveTrivialQP(
     Slice!(const(T)*) q,
     Slice!(T*) x,
     Slice!(T*) y,
+
     Slice!(T*) z,
     Slice!(T*) work,
     scope void delegate(
@@ -428,7 +758,10 @@ do {
     auto n = x.length;
     auto xPrev = work[n * 0 .. n * 1];
     auto yPrev = work[n * 1 .. n * 2];
+    x[] = 0;
+    y[] = 0;
     z[] = x;
+    auto sigma = 1e-2;
     int rhoAge;
     T qMax = T(0).reduce!fmax(map!fabs(q));
     T rho = settings.initialRho;
@@ -437,8 +770,8 @@ do {
         rho = rho.fmin(maxRho).fmax(minRho);
         xPrev[] = x;
         yPrev[] = y;
-
-        x[] = 1.00000 * (1 / (1 + rho)) * (rho * z - yPrev - q);
+        x *= sigma;
+        x[] = 1.00000 * (1 / (1 + sigma + rho)) * (sigma * x + rho * z - yPrev - q);
         y[] = x + (1 - 1.00000) * z + 1 / rho * yPrev;
         x[] += (1 - 1.00000) * xPrev;
         // debug writeln(i, " y' = ", y);
@@ -449,6 +782,13 @@ do {
         debug writeln(i, " y = ", y);
         debug writeln(i, " z = ", z);
         debug writeln(i, " rho = ", rho);
+        auto v = [-1.27373082,  1.50000355, -2.15119364].sliced;
+        auto w = new T[n].sliced;
+        projection(v, w);
+        debug writeln(i, " w = ", w);
+        debug writeln(i, " rw = ", T(0).reduce!fmax(map!fabs(v - w)));
+            
+
 
         T xMax = T(0).reduce!fmax(map!fabs(x));
         T yMax = T(0).reduce!fmax(map!fabs(y));
