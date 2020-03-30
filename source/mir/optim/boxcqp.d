813fa0800bf84897ca5703026f7b0a1b7e790b95
9il <ilyayaroshenko@gmail.com>
2020-03-30 10:18:52 +0700

    fixup

--- source/mir/optim/boxcqp.d
+++ source/mir/optim/boxcqp.d
@@ -76,8 +76,8 @@ Solves:
 Params:
     P = Positive-definite Matrix, NxN
     q = Linear component, N
-    l = Lower bounds in `[-inf, +inf)`, N
-    u = Upper bounds in `(-inf, +inf]`, N
+    l = Lower bounds in `[-inf, +inf$(RPAREN)`, N
+    u = Upper bounds in `$(LPAREN)-inf, +inf]`, N
     x = solutoin, N
     settings = Iteration settings (optional)
 +/
@@ -110,8 +110,8 @@ Params:
         The upper triangular part (and diagonal) of the matrix is used for temporary data and then can be resotored.
         Matrix diagonal is always restored.
     q = Linear component, N
-    l = Lower bounds in `[-inf, +inf)`, N
-    u = Upper bounds in `(-inf, +inf]`, N
+    l = Lower bounds in `[-inf, +inf$(RPAREN)`, N
+    u = Upper bounds in `$(LPAREN)-inf, +inf]`, N
     x = solutoin, N
     unconstrainedSolution = 
     work = workspace, $(LREF mir_box_qp_work_length)(N)
